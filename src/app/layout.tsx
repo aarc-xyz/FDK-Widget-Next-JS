94449b58ad7159cc11ea8a2b44ab3d77c200c65b
amagnum <bagheladarsh5@gmail.com>
2024-11-22 14:27:58 +0530

    chore: Update AarcProvider configuration and dependencies, and add ContractExecution component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import dynamic from "next/dynamic";
+import ContractExecution from "./executeContract";
 
 const AarcProvider = dynamic(() => import("../contexts/AarcProvider"), {
   ssr: false,
@@ -20,9 +21,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>
         <AarcProvider>{children}</AarcProvider>
+
+        <ContractExecution />
       </body>
     </html>
   );
