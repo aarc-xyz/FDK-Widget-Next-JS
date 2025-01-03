b5cc0ccd77be999dba743b9b58cd87a457b43be4
amagnum <bagheladarsh5@gmail.com>
2025-01-03 18:06:35 +0530

    fix: update comment in AarcEthersEthereumSigner to clarify blockChainExplorerUrl usage

--- src/utils/CustomWalletConnectorWithEthers.tsx
+++ src/utils/CustomWalletConnectorWithEthers.tsx
@@ -237,7 +237,7 @@ export class AarcEthersEthereumSigner {
 
     /**
      * Optional: track a "current" block explorer URL if you want to store it
-     * after switching to a known chain. If not needed, you can remove.
+     * after switching to a known chain. If not needed, you can remove. it helps internal UI to get the block explorer URL.
      */
     public blockChainExplorerUrl?: string
 
